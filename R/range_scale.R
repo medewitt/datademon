e55be7fa5e5043025c298e4ff67cf63ff03f3cbe
medewitt <me.dewitt.jr@gmail.com>
2018-01-09 10:01:47 -0500

    added documentation

--- R/range_scale.R
+++ R/range_scale.R
@@ -1,6 +1,7 @@
 #' A function to scale values based on ranges rather than typical R scale function
-
+#' @param df which contains the numeric values to be scaled via range
 #'
+#' @return df with the scaled values
 
 
 range_scale <- function(df){
